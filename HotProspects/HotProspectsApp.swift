d2d484880609aacd9e210f821df19d1ef34cefe8
yerdi <yerdi.isman@gmail.com>
2023-01-25 23:47:01 +0600

    first commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Yerdaulet Ismanaliyev on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
